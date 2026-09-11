fd2d9486054342a551cfeb8182825a0bae3e4f6f
Hani <wintrix0101@gmail.com>
2026-09-11 13:15:34 +0200

    Add project files.

---
+++ Uppgift 2.4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace övning2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hur många dagar ska du hyra bilen");
+            double dagar = double.Parse(Console.ReadLine());
+            Console.WriteLine("Hur många Kilometer ska du köra med bilen");
+            double km = double.Parse(Console.ReadLine());
+            double summa = 300 + km + (500 * (dagar - 1));
+            Console.WriteLine("den total kostnaden är " + summa);
+        }
+    }
+}
